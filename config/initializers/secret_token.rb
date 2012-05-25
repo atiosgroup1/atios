256f0f7215bbab6ae43b82eaf9d0fbe695170dcf
Vladimir Makarov <vladimir.makarov.il@gmail.com>
2012-05-25 21:00:56 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atios::Application.config.secret_token = '9975cda4cb9707b4aeb89207039eb63895f2eeba0bfe61f61ce9cefb315f4599eaa0fdde6df1ab5f8c3c3d4fea4716bd5858464e1967a11a17ddc70000938aa8'
