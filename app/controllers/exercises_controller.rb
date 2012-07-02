5044194a9f7dc12a59a28bc54e89dfd50184ba30
Vladimir Makarov <vladimir.makarov.il@gmail.com>
2012-07-02 10:35:56 +0300

    5

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,17 +9,17 @@ class ExercisesController < ApplicationController
 
   def create
     @course = Course.find_by_id(params[:exercise][:course_id])
-    Dir.mkdir("public/data")
+    Dir.mkdir("public/data") unless Dir.exist?("public/data")
     @directory = "public/data/"+SecureRandom.uuid
     Dir.mkdir(@directory)
     @exercise_file_name = File.join(@directory, "exercise_" + params[:exercise][:title] +SecureRandom.uuid + params[:exercise][:exercise_path].original_filename)
     @script_file_name = File.join(@directory,"script_" + params[:exercise][:title] +SecureRandom.uuid + params[:exercise][:script_path].original_filename)
     File.open(@exercise_file_name, "wb") {|f| f.write params[:exercise][:exercise_path].read }
     File.open(@script_file_name, "wb") {|f| f.write params[:exercise][:script_path].read }
-
+    p params[:exercise]["due_date(1i)"].inspect
     @exercise = @course.exercises.create(
        :description => params[:exercise][:description],
-       :due_date => Date.new(),
+       :due_date => 2.weeks.from_now,
        :exercise_path=>@exercise_file_name[7..-1],
        :script_path=>@script_file_name[7..-1],
        :title=>params[:exercise][:title])
@@ -34,6 +34,13 @@ class ExercisesController < ApplicationController
   end
 
   def destroy
-
+    @exercise = Exercise.find_by_id(params[:id])
+    @course = Course.find_by_id(@exercise.course_id)
+    if @exercise.delete
+      flash[:success] = "Exercise deleted"
+    else
+      flash[:error] = "Failed to delete exercise"
+    end
+    redirect_to course_path @course
   end
 end
